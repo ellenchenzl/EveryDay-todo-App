c8aa4626a87a63313fcfc81e26d3dda5f61c04a2
Chen Zhuoling <32111322+chenz6@users.noreply.github.com>
2020-01-05 16:22:58 -0800

    add features for home page

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   todoArray=[] }
+   todoArray=[] 
+   addTodo(value){    
+   this.todoArray.push(value)    
+    }}
