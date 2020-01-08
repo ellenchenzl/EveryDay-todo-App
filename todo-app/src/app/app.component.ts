22490fc6187a697f1188b75e4bef1f47b6878c9a
Chen Zhuoling <32111322+chenz6@users.noreply.github.com>
2020-01-07 18:38:51 -0800

    change the CSS file, Fix the delet bug.

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   todoArray=[] 
+   todoArray=[] ;
+
+   todo;
+
    addTodo(value){    
-   this.todoArray.push(value)    
-    }}
+   if (value !=""){
+   		this.todoArray.push(value)  
+   		}
+   else{
+   		alert(" List cannot be Empty!")
+   		}
+   }
+
+   deleteItem(todo){
+  		for(let i=0;i<todo.length;i++){
+  			if (todo == this.todoArray[i]){
+
+  			this.todoArray.splice(i,1)	
+  			}
+  		} 
+   } }
