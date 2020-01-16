aa1998a35d5342dfe7e2130f555816c84d112d28
Chen Zhuoling <32111322+chenz6@users.noreply.github.com>
2020-01-16 10:56:34 -0800

    delete the diary and about page

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -5,28 +5,25 @@ import {RouterModule,Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiaryComponent } from './diary/diary.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
+
 
 
 
 const appRoutes:Routes=[
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'Home',component: HomeComponent}, 
-  {path:'Diary',component: DiaryComponent},
   {path:'Contact',component: ContactComponent},
-  {path:'About',component: AboutComponent},
+ 
   ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaryComponent,
     HomeComponent,
-    ContactComponent,
-    AboutComponent
+    ContactComponent
+  
   ],
   imports: [
     RouterModule.forRoot(
